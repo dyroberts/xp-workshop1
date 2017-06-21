06791f4bd25d7d8a3553b7a666029472d64866e9
Dylan Roberts <dyroberts@pivotal.io>
2017-06-21 11:13:52 -0400

    finish kata

--- xp-workshop1/src/io/pivotal/rps/RpsKata.java
+++ xp-workshop1/src/io/pivotal/rps/RpsKata.java
@@ -1,7 +1,11 @@
 package io.pivotal.rps;
 
 import static io.pivotal.rps.RpsKata.Result.P1;
+import static io.pivotal.rps.RpsKata.Result.P2;
 import static io.pivotal.rps.RpsKata.Result.TIE;
+import static io.pivotal.rps.RpsKata.Throw.PAPER;
+import static io.pivotal.rps.RpsKata.Throw.ROCK;
+import static io.pivotal.rps.RpsKata.Throw.SCISSORS;
 
 public class RpsKata {
 
@@ -9,16 +13,23 @@ public class RpsKata {
         if (p1Throws == p2Throws) {
             return TIE;
         }
+        if (p1Throws == ROCK && p2Throws == PAPER ||
+                p1Throws == PAPER && p2Throws == SCISSORS ||
+                p1Throws == SCISSORS && p2Throws == ROCK) {
+            return P2;
+        }
         return P1;
     }
 
     public enum Result {
         P1,
+        P2,
         TIE
     }
 
     public enum Throw {
         ROCK,
+        PAPER,
         SCISSORS
     }
 }
--- xp-workshop1/src/io/pivotal/rps/RpsKataTest.java
+++ xp-workshop1/src/io/pivotal/rps/RpsKataTest.java
@@ -3,7 +3,9 @@ package io.pivotal.rps;
 import org.junit.Test;
 
 import static io.pivotal.rps.RpsKata.Result.P1;
+import static io.pivotal.rps.RpsKata.Result.P2;
 import static io.pivotal.rps.RpsKata.Result.TIE;
+import static io.pivotal.rps.RpsKata.Throw.PAPER;
 import static io.pivotal.rps.RpsKata.Throw.ROCK;
 import static io.pivotal.rps.RpsKata.Throw.SCISSORS;
 import static org.junit.Assert.assertEquals;
@@ -16,7 +18,42 @@ public class RpsKataTest {
     }
 
     @Test
+    public void whenP1ThrowsPaperAndP2ThrowsRock_P1ShouldWin() {
+        assertEquals(P1, RpsKata.play(PAPER, ROCK));
+    }
+
+    @Test
+    public void whenP1ThrowsScissorsAndP2ThrowsPaper_P1ShouldWin() {
+        assertEquals(P1, RpsKata.play(SCISSORS, PAPER));
+    }
+
+    @Test
     public void whenP1ThrowsRockAndP2ThrowsRock_ItShouldBeATie() {
         assertEquals(TIE, RpsKata.play(ROCK, ROCK));
+    }
+
+    @Test
+    public void whenP1ThrowsPaperAndP2ThrowsPaper_ItShouldBeATie() {
+        assertEquals(TIE, RpsKata.play(PAPER, PAPER));
+    }
+
+    @Test
+    public void whenP1ThrowsScissorsAndP2ThrowsScissors_ItShouldBeATie() {
+        assertEquals(TIE, RpsKata.play(SCISSORS, SCISSORS));
+    }
+
+    @Test
+    public void whenP1ThrowsRockAndP2ThrowsPaper_P2ShouldWin() {
+        assertEquals(P2, RpsKata.play(ROCK, PAPER));
+    }
+
+    @Test
+    public void whenP1ThrowsPaperAndP2ThrowsScissors_P2ShouldWin() {
+        assertEquals(P2, RpsKata.play(PAPER, SCISSORS));
+    }
+
+    @Test
+    public void whenP1ThrowsScissorsAndP2ThrowsRock_P2ShouldWin() {
+        assertEquals(P2, RpsKata.play(SCISSORS, ROCK));
     }
 }
